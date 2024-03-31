b3665bd77df34a4e890dc4c82e16155817d9cc78
Dilnawaz Khan <dilkhan0706@gmail.com>
2024-03-31 21:13:39 +0500

    textinput changes

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,5 +1,6 @@
-import {StyleSheet, Text, TextInput, View} from 'react-native';
 import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+import {Fonts} from '../../constants';
 import {Colors} from '../../constants/colors';
 
 interface SearchBarProps {
@@ -9,30 +10,26 @@ interface SearchBarProps {
 
 const SearchBar = ({value, onChangeText}: SearchBarProps) => {
   return (
-    <View style={styles.container}>
-      <TextInput
-        value={value}
-        onChangeText={onChangeText}
-        placeholder="Enter Your City Name..."
-        placeholderTextColor={Colors.gray}
-        style={styles.searchInput}
-      />
-    </View>
+    <TextInput
+      value={value}
+      onChangeText={onChangeText}
+      placeholder="Enter Your City Name..."
+      placeholderTextColor={Colors.gray}
+      style={styles.searchInput}
+    />
   );
 };
 
 export {SearchBar};
 
 const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: 40,
-  },
   searchInput: {
     backgroundColor: Colors.lightGray,
     width: '100%',
-    height: '100%',
+    height: 40,
     borderRadius: 5,
     paddingHorizontal: 10,
+    color: Colors.black,
+    fontFamily: Fonts.Regular,
   },
 });
